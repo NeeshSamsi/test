1fc64269d37d7dd4def8242c7a37f16f69c48a0e
Neesh <avaneeshsamsi@gmail.com>
2023-06-15 16:26:32 +0530

    Keystatic setup

---
+++ keystatic.config.ts
@@ -0,0 +1,64 @@
+import { config, fields, singleton } from "@keystatic/core"
+
+export default config({
+  storage: {
+    kind: "github",
+    repo: {
+      owner: "NeeshSamsi",
+      name: "test",
+    },
+  },
+  singletons: {
+    homepage: singleton({
+      label: "Home Page",
+      path: "src/data/pages/home",
+      schema: {
+        headline: fields.text({
+          label: "Headline - Main section",
+          description: "The main underlined headline shown in the main section.",
+          validation: {
+            length: { min: 1 },
+          },
+        }),
+        subheadline: fields.text({
+          label: "Sub headline - Main section",
+          description: "The sub headline shown under the main headline.",
+          validation: {
+            length: { min: 1 },
+          },
+        }),
+        backgroundImage: fields.image({
+          label: "Background Image - Main section",
+          description: "Background image for the Main Section.",
+          validation: {
+            isRequired: true,
+          },
+          directory: "/public/images/home",
+          publicPath: "/images/home",
+        }),
+
+        // storyHeadline: fields.text({
+        //   label: "Headline - Story section",
+        //   description: "Shows as the heading of the Story Section.",
+        //   validation: {
+        //     length: { min: 1 },
+        //   },
+        // }),
+        // storyDescription: fields.document({
+        //   label: "Content - Story section",
+        //   description: "Shows under Story Headline.",
+        //   formatting: true,
+        // }),
+        // storyImage: fields.image({
+        //   label: "Image - Story section",
+        //   description: "Image that shows side by side with the Story section.",
+        //   validation: {
+        //     isRequired: true,
+        //   },
+        //   directory: "/public/images/home",
+        //   publicPath: "/images/home",
+        // }),
+      },
+    }),
+  },
+})
